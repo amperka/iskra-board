7c2139454af20eb682dda442cf78af63eee238cb
Yury Botov <yury.botov@gmail.com>
2019-09-10 16:54:43 +0300

    Clear iskrajs variant.cpp
    Remove extra comments and add banner

--- packages/AmperkaARM/AmperkaARM/variants/iskrajs/variant.cpp
+++ packages/AmperkaARM/AmperkaARM/variants/iskrajs/variant.cpp
@@ -1,14 +1,11 @@
 /*
- *******************************************************************************
- * Copyright (c) 2019, STMicroelectronics
- * All rights reserved.
+ * This file is a part of Amperka-boards cores.
  *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- *******************************************************************************
+ * Defines: Iskra JS board definitions.
+ * © Amperka LLC (https://amperka.com, dev@amperka.com)
+ * 
+ * Author: Yury Botov <by@amperka.ru>
+ * License: GPLv3, all text here must be included in any redistribution.
  */
 
 #include "pins_arduino.h"
@@ -17,130 +14,94 @@
 extern "C" {
 #endif
 
-// Pin number
-// This array allows to wrap Arduino pin number(Dx or x)
-// to STM32 PinName (PX_n)
 const PinName digitalPin[] = {
-  //PX_n, //Dx
-  // Arduino "digital" pins
-  PB_11, //rx
-  PB_10, //tx
-  PA_6,
-  PA_7,
-  PC_3,
-  PB_1,
-  PB_0,
-  PC_2,
-  PC_6,
-  PC_7,
-  PC_8,
-  PC_9,
-  PA_8,
-  PA_10,
-  // Arduino i2c extension
-  PB_9, //sda
-  PB_8, //scl
-  // Arduino "upload" 6-pin
-  PB_12, //cs
-  PB_13, //sck
-  PB_14, //miso
-  PB_15, //mosi
-  // Iskra extra
-  PB_6, //led1
-  PB_7, //led "busy"
-  PC_4, //user button 
-  // Arduino "analog" pins
-  PA_0,
-  PA_1,
-  PA_2,
-  PA_3,
-  PA_4,
-  PA_5,
-  // Duplicate for ADC using
-  PA_0,
-  PA_1,
-  PA_2,
-  PA_3,
-  PA_4,
-  PA_5,
-  // ADC in "digital" pin area
-  PA_6,
-  PA_7,
-  PC_3,
-  PB_1,
-  PB_0,
-  PC_2
+    // Arduino "digital" pins
+    PB_11, //rx
+    PB_10, //tx
+    PA_6,
+    PA_7,
+    PC_3,
+    PB_1,
+    PB_0,
+    PC_2,
+    PC_6,
+    PC_7,
+    PC_8,
+    PC_9,
+    PA_8,
+    PA_10,
+    // Arduino i2c extension
+    PB_9, //sda
+    PB_8, //scl
+    // Arduino "upload" 6-pin
+    PB_12, //cs
+    PB_13, //sck
+    PB_14, //miso
+    PB_15, //mosi
+    // Iskra extra
+    PB_6, //led1
+    PB_7, //led "busy"
+    PC_4, //user button
+    // Arduino "analog" pins
+    PA_0,
+    PA_1,
+    PA_2,
+    PA_3,
+    PA_4,
+    PA_5,
+    // Duplicate for ADC using
+    PA_0,
+    PA_1,
+    PA_2,
+    PA_3,
+    PA_4,
+    PA_5,
+    // ADC in "digital" pin area
+    PA_6,
+    PA_7,
+    PC_3,
+    PB_1,
+    PB_0,
+    PC_2
 };
 
 #ifdef __cplusplus
 }
 #endif
 
-// ----------------------------------------------------------------------------
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow :
-  *            System Clock source            = PLL (HSE)
-  *            SYSCLK(Hz)                     = 168000000
-  *            HCLK(Hz)                       = 168000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 4
-  *            APB2 Prescaler                 = 2
-  *            HSE Frequency(Hz)              = 8000000
-  *            PLL_M                          = 8
-  *            PLL_N                          = 336
-  *            PLL_P                          = 2
-  *            PLL_Q                          = 7
-  *            VDD(V)                         = 3.3
-  *            Main regulator output voltage  = Scale1 mode
-  *            Flash Latency(WS)              = 5
-  * @param  None
-  * @retval None
-  */
-WEAK void SystemClock_Config(void)
-{
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
+/* System Clock Configuration 168MHz with 8MHz quarz */
 
-  /* Enable Power Control clock */
-  __HAL_RCC_PWR_CLK_ENABLE();
+WEAK void SystemClock_Config(void) {
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
 
-  /* The voltage scaling allows optimizing the power consumption when the device is
-     clocked below the maximum system frequency, to update the voltage scaling value
-     regarding system frequency refer to product datasheet.  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /* Enable HSE Oscillator and activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-     clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
-                                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
 
-  /* STM32F405x/407x/415x/417x Revision Z devices: prefetch is supported  */
-  if (HAL_GetREVID() == 0x1001) {
-    /* Enable the Flash prefetch */
-    __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
-  }
+    if (HAL_GetREVID() == 0x1001) {
+        __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+    }
 }
 
 #ifdef __cplusplus
