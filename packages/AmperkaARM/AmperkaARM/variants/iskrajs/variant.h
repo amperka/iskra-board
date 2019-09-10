02ae4e244be2d63387fdd460c57257b25e55005b
Yury Botov <yury.botov@gmail.com>
2019-09-10 16:49:23 +0300

    Clear iskrajs variant.h
    remove extra comments and place banner

--- packages/AmperkaARM/AmperkaARM/variants/iskrajs/variant.h
+++ packages/AmperkaARM/AmperkaARM/variants/iskrajs/variant.h
@@ -1,14 +1,11 @@
 /*
- *******************************************************************************
- * Copyright (c) 2019, STMicroelectronics
- * All rights reserved.
+ * This file is a part of Amperka-boards cores.
  *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- *******************************************************************************
+ * Defines: Iskra JS board definitions.
+ * © Amperka LLC (https://amperka.com, dev@amperka.com)
+ * 
+ * Author: Yury Botov <by@amperka.ru>
+ * License: GPLv3, all text here must be included in any redistribution.
  */
 
 #ifndef _VARIANT_AMPERKA_STM32_
@@ -22,160 +19,72 @@ extern "C" {
  *        Pins
  *----------------------------------------------------------------------------*/
 
-// Define pin names to match digital pin number --> Dx
-// It could be used with preprocessor tests (e.g. #if PXn == 3)
-// so an enum will not work.
-// !!!
-// !!! Copy the digitalPin[] array from the variant.cpp
-// !!! and remove all '_': PX_n --> PXn
-// !!! For NC, comment the line to warn x pin number is NC
-// !!! // x is NC
-// !!! For duplicated pin name, comment the line to warn x pin number
-// !!! is PXn which is already defined with y pin number
-// !!! // x is PXn (y)
-// !!! Ex:
-// !!! ...
-// !!! #define PA4  20 // A14
-// !!! #define PB4  21
-// !!! #define PB5  22
-// !!! #define PB3  23
-// !!! // 24 is PA4 (20)
-// !!! // 25 is PB4 (21)
-// !!! #define PA2  26 // A15
-// !!! ...
+// Arduino "digital" pins
+#define PB11 0
+#define PB10 1
+#define PA6 2
+#define PA7 3
+#define PC3 4
+#define PB1 5
+#define PB0 6
+#define PC2 7
+#define PC6 8
+#define PC7 9
+#define PC8 10
+#define PC9 11
+#define PA8 12
+#define PA10 13
+// Arduino i2c extension
+#define PB9 14
+#define PB8 15
+// Arduino "upload" 6-pin
+#define PB12 16
+#define PB13 17
+#define PB14 18
+#define PB15 19
+// Iskra extra
+#define PB6 20
+#define PB7 21
+#define PC4 22
+// Arduino "analog" pins
+#define PA0 23
+#define PA1 24
+#define PA2 25
+#define PA3 26
+#define PA4 27
+#define PA5 28
 
-  // Arduino "digital" pins
-  #define PB11 0
-  #define PB10 1
-  #define PA6 2
-  #define PA7 3
-  #define PC3 4
-  #define PB1 5
-  #define PB0 6
-  #define PC2 7
-  #define PC6 8
-  #define PC7 9
-  #define PC8 10
-  #define PC9 11
-  #define PA8 12
-  #define PA10 13
-  // Arduino i2c extension
-  #define PB9 14
-  #define PB8 15
-  // Arduino "upload" 6-pin
-  #define PB12 16
-  #define PB13 17
-  #define PB14 18
-  #define PB15 19
-  // Iskra extra
-  #define PB6 20
-  #define PB7 21
-  #define PC4 22 
-  // Arduino "analog" pins
-  #define PA0 23
-  #define PA1 24
-  #define PA2 25
-  #define PA3 26
-  #define PA4 27
-  #define PA5 28
-
-// This must be a literal
-// It is used with preprocessor tests (e.g. #if NUM_DIGITAL_PINS > 3)
-// so an enum will not work.
-#define NUM_DIGITAL_PINS        42
-
-// Allow to define Arduino style alias for analog input pin number --> Ax
-// All pins are digital, analog inputs are a subset of digital pins
-// and must be contiguous to be able to loop on each value
-// This must be a literal with a value less than or equal to MAX_ANALOG_INPUTS
-// defined in pin_arduino.h
-// It is used with preprocessor tests (e.g. #if NUM_ANALOG_INPUTS > 3)
-// so an enum will not work.
-// !!!
-// !!! It must be aligned with the number of analog PinName
-// !!! defined in digitalPin[] array in variant.cpp
-// !!!
-#define NUM_ANALOG_INPUTS       12
-// Define digital pin number of the first analog input  (i.e. which digital pin is A0)
-// First analog pin value (A0) must be greater than or equal to NUM_ANALOG_INPUTS
-#define NUM_ANALOG_FIRST        30
-
-// Below ADC, DAC and PWM definitions already done in the core
-// Could be redefined here if needed
-// ADC resolution is 12bits
-//#define ADC_RESOLUTION          12
-//#define DACC_RESOLUTION         12
-
-// PWM resolution
-//#define PWM_RESOLUTION          8
-//#define PWM_FREQUENCY           1000
-//#define PWM_MAX_DUTY_CYCLE      255
+#define NUM_DIGITAL_PINS 42
+#define NUM_ANALOG_INPUTS 12
+#define NUM_ANALOG_FIRST 30
 
 // On-board LED pin number
-#define LED_BUILTIN             PB6
-#define LED_GREEN               PB7
+#define LED_BUILTIN PB6
+#define LED_GREEN PB7
 
 // On-board user button
-#define USER_BTN                PC4
+#define USER_BTN PC4
 
-// Below SPI and I2C definitions already done in the core
-// Could be redefined here if differs from the default one
 // SPI Definitions
-#define PIN_SPI_SS              PB12
-#define PIN_SPI_MOSI            PB15
-#define PIN_SPI_MISO            PB14
-#define PIN_SPI_SCK             PB13
+#define PIN_SPI_SS PB12
+#define PIN_SPI_MOSI PB15
+#define PIN_SPI_MISO PB14
+#define PIN_SPI_SCK PB13
 
 // I2C Definitions
-#define PIN_WIRE_SDA            PB9
-#define PIN_WIRE_SCL            PB8
-
-// I2C timing definitions (optional), avoid time spent to compute if defined        TODO
-// * I2C_TIMING_SM for Standard Mode (100kHz)
-// * I2C_TIMING_FM for Fast Mode (400kHz)
-// * I2C_TIMING_FMP for Fast Mode Plus (1000kHz)
-//#define I2C_TIMING_SM           0x00000000
-//#define I2C_TIMING_FM           0x00000000
-//#define I2C_TIMING_FMP          0x00000000
+#define PIN_WIRE_SDA PB9
+#define PIN_WIRE_SCL PB8
 
 // Timer Definitions (optional)
-//Do not use timer used by PWM pins when possible. See PinMap_PWM in PeripheralPins.c
-#define TIMER_TONE              TIM6
 
-// Do not use basic timer: OC is required
-#define TIMER_SERVO             TIM2  //TODO: advanced-control timers don't work
+#define TIMER_TONE TIM6
+#define TIMER_SERVO TIM2
 
 // UART Definitions
-// Define here Serial instance number to map on Serial generic name
-#define SERIAL_UART_INSTANCE    2 //Connected to ST-Link
-// DEBUG_UART could be redefined to print on another instance than 'Serial'
-//#define DEBUG_UART              ((USART_TypeDef *) U(S)ARTX) // ex: USART3
-// DEBUG_UART baudrate, default: 9600 if not defined
-//#define DEBUG_UART_BAUDRATE     x
-// DEBUG_UART Tx pin name, default: the first one found in PinMap_UART_TX for DEBUG_UART
-//#define DEBUG_PINNAME_TX        PX_n // PinName used for TX
+#define SERIAL_UART_INSTANCE 2 //Connected to ST-Link
 
-// Default pin used for 'Serial' instance (ex: ST-Link)
-// Mandatory for Firmata
-// UART Definitions
-
-#define PIN_SERIAL_RX           PA3
-#define PIN_SERIAL_TX           PA2
-
-// Optional PIN_SERIALn_RX and PIN_SERIALn_TX where 'n' is the U(S)ART number
-// Used when user instanciate a hardware Serial using its peripheral name.
-// Example: HardwareSerial mySerial(USART3);
-// will use PIN_SERIAL3_RX and PIN_SERIAL3_TX if defined.
-//#define PIN_SERIALn_RX          x // For U(S)ARTn RX
-//#define PIN_SERIALn_TX          x // For U(S)ARTn TX
-//#define PIN_SERIALLP1_RX        x // For LPUART1 RX
-//#define PIN_SERIALLP1_TX        x // For LPUART1 TX
-
-// SD card slot Definitions
-// SD detect signal can be defined if required
-//#define SD_DETECT_PIN           x
-// SD Read/Write timeout, default value defined in STM32SD library
-//#define SD_DATATIMEOUT          x
+#define PIN_SERIAL_RX PA3
+#define PIN_SERIAL_TX PA2
 
 #ifdef __cplusplus
 } // extern "C"
@@ -185,10 +94,6 @@ extern "C" {
  *----------------------------------------------------------------------------*/
 
 #ifdef __cplusplus
-// These serial port names are intended to allow libraries and architecture-neutral
-// sketches to automatically default to the correct port name for a particular type
-// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
-// the first hardware serial port whose RX/TX pins are not dedicated to another use.
 //
 // SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
 //
@@ -200,8 +105,8 @@ extern "C" {
 //
 // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
 //                            pins are NOT connected to anything by default.
-#define SERIAL_PORT_MONITOR     Serial
-#define SERIAL_PORT_HARDWARE    Serial1
+#define SERIAL_PORT_MONITOR Serial
+#define SERIAL_PORT_HARDWARE Serial1
 #endif
 
 // HAL definitions
@@ -226,5 +131,3 @@ extern "C" {
 #define HAL_SD_MODULE_ENABLED
 
 #endif /* _VARIANT_AMPERKA_STM32_ */
-
-
