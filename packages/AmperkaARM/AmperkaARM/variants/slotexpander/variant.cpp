7a5f14f288716dbc9006278a301006e3e2b353c0
Yury Botov <yury.botov@gmail.com>
2019-09-10 17:03:02 +0300

    Clear Slot Expander variant.cpp
    as previous

--- packages/AmperkaARM/AmperkaARM/variants/slotexpander/variant.cpp
+++ packages/AmperkaARM/AmperkaARM/variants/slotexpander/variant.cpp
@@ -1,14 +1,11 @@
 /*
- *******************************************************************************
- * Copyright (c) 2019, STMicroelectronics
- * All rights reserved.
+ * This file is a part of Amperka-boards cores.
  *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- *******************************************************************************
+ * Defines: Slot Expander board definitions.
+ * © Amperka LLC (https://amperka.com, dev@amperka.com)
+ * 
+ * Author: Yury Botov <by@amperka.ru>
+ * License: GPLv3, all text here must be included in any redistribution.
  */
 
 #include "pins_arduino.h"
@@ -18,81 +15,67 @@ extern "C" {
 #endif
 
 // Pin number
-// This array allows to wrap Arduino pin number(Dx or x)
-// to STM32 PinName (PX_n)
 const PinName digitalPin[] = {
-  //PX_n, //Dx
-  PB_1, //D0
-  PA_7, //D1
-  PA_6, //D2
-  PA_5, //D3
-  PA_0, //D4
-  PA_1, //D5
-  PA_2, //D6/TX
-  PA_4, //D7
-  PA_3, //D8/RX
-  PF_0, //D9
-  PF_1, //D10/LED
-  PA_9, //D11/SCL
-  PA_10, //D12/SDA
-  // duplicate for analog input pins
-  PB_1, //D13/A0
-  PA_7, //D14/A1
-  PA_6, //D15/A2
-  PA_5, //D16/A3
-  PA_0, //D17/A4
-  PA_1, //D18/A5
-  PA_4, //D19/A7
-  PA_3, //D20/A8
+    PB_1, //D0
+    PA_7, //D1
+    PA_6, //D2
+    PA_5, //D3
+    PA_0, //D4
+    PA_1, //D5
+    PA_2, //D6/TX
+    PA_4, //D7
+    PA_3, //D8/RX
+    PF_0, //D9
+    PF_1, //D10/LED
+    PA_9, //D11/SCL
+    PA_10, //D12/SDA
+    // duplicate for analog input pins
+    PB_1, //D13/A0
+    PA_7, //D14/A1
+    PA_6, //D15/A2
+    PA_5, //D16/A3
+    PA_0, //D17/A4
+    PA_1, //D18/A5
+    PA_4, //D19/A7
+    PA_3, //D20/A8
 };
 
 #ifdef __cplusplus
 }
 #endif
 
-// ----------------------------------------------------------------------------
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/**
-  * @brief  System Clock Configuration
-  * @param  None
-  * @retval None
-  */
-WEAK void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+/* System Clock Configuration */
+WEAK void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI14;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.HSI14CalibrationValue = 16;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
-  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    while(1);
-  }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI14;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.HSI14CalibrationValue = 16;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+    RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        while (1)
+            ;
+    }
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+        | RCC_CLOCKTYPE_PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    while(1);
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+        while (1)
+            ;
+    }
 }
 
 #ifdef __cplusplus
