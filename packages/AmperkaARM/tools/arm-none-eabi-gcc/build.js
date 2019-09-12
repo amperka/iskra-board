c0a11388b40c5b14393bfa50041d38e7ef5384a8
Yury Botov <yury.botov@gmail.com>
2019-09-12 11:00:15 +0300

    Windows GCC archive must be repacking

--- packages/AmperkaARM/tools/arm-none-eabi-gcc/build.js
+++ packages/AmperkaARM/tools/arm-none-eabi-gcc/build.js
@@ -39,9 +39,15 @@ config.forEach(function(cfg) {
                                 releasesPath + '/' + cfg.target + '/' +
                                 cfg['storage-name'],
                             function(error, stdout, stderr) {
-                              console.log(
+                              if(error)
+                                console.log(stdout+stderr);
+                              else 
+                                {
+                                  console.log(stdout);
+                                  console.log(
                                   'Done: ' + releasesPath + '/' + cfg.target +
                                   '/' + cfg['storage-name']);
+                                }
                             });
                       });
                     });
@@ -72,12 +78,22 @@ config.forEach(function(cfg) {
                         child_process.exec(
                             'cp /tmp/' + process.argv[2] + '.' + cfg.target +
                                 '.tar.bz2 ' + releasesPath + '/' + cfg.target +
-                                '/' + cfg['storage-name'],
+                                '/' + cfg['storage-name'] + '; ' +
+                            'rm -f /tmp/' + process.argv[2] + '.' + cfg.target + '.tar.bz2 ;' +
+                            'rm -f /tmp/' + process.argv[2] + '.' + cfg.target + '.html ;'
+                            ,
                             function(error, stdout, stderr) {
-                              console.log(
+                              if(error)
+                                console.log(stdout+stderr);
+                              else 
+                                {
+                                  console.log(stdout);
+                                  console.log(
                                   'Done: ' + releasesPath + '/' + cfg.target +
                                   '/' + cfg['storage-name']);
-                            });
+                                }
+                            }
+                        );
                       });
                     });
                   });
@@ -86,15 +102,74 @@ config.forEach(function(cfg) {
         });
       });
       break;
+      case 'repack-from-github-releases':
+        var linkFile = fs.createWriteStream(
+            '/tmp/' + process.argv[2] + '.' + cfg.target + '.html');
+        var file = fs.createWriteStream(
+            '/tmp/arm-gcc.zip');
+        var request = https.get(cfg['remote-url'], function(linkResponse) {
+          linkResponse.pipe(linkFile);
+          linkFile.on('finish', function() {
+            fs.readFile(
+                '/tmp/' + process.argv[2] + '.' + cfg.target + '.html',
+                function(err, data) {
+                  if (!err) {
+                    var m = data.toString().match(/\<a href\=\"(.+)"\>/);
+                    console.log('Real path: ' + m[1]);
+                    var request = https.get(m[1], function(response) {
+                      response.pipe(file);
+                      file.on('finish', function() {
+                        file.close(function() {
+                          child_process.exec(
+                              'cd /tmp ; ' +
+                              'mkdir -p /tmp/arm-none-eabi-gcc ; '+
+                              'echo mkdir done ;' +
+                              'unzip -uo /tmp/arm-gcc.zip -d /tmp/arm-none-eabi-gcc ; ' +
+                              'echo unzip done ;' +
+                              'tar -cjpf ' + releasesPath + '/' + cfg.target + '/' + cfg['storage-name'] + ' arm-none-eabi-gcc/* ; ' +
+                              'echo tar done ;' +
+                              'rm -f /tmp/arm-gcc.zip ; ' +
+                              'echo rm done ;' +
+                              'rm -Rf /tmp/arm-none-eabi-gcc ;' +
+                              'echo rmdir done ;'
+                              ,                       
+                              function(error, stdout, stderr) {
+                                if(error)
+                                  console.log(stdout+stderr);
+                                else 
+                                  {
+                                    console.log(stdout);
+                                    console.log(
+                                      'Done: ' + releasesPath + '/' + cfg.target +
+                                      '/' + cfg['storage-name']);
+                                  }
+                              });
+                        });
+                      });
+                    });
+                  }
+                });
+          });
+        });
+        break;
     case 'repac-from-github-lfs':
       child_process.exec(
-          'mkdir /tmp/cmsis; cd /tmp/cmsis; git lfs clone ' +
-              cfg['remote-url'] + ';tar -cjpf ' + releasesPath + '/' +
-              cfg.target + '/' + cfg['storage-name'] + ' CMSIS_5/*',
+          'mkdir /tmp/cmsis ; ' +
+          'cd /tmp/cmsis ; ' +
+          'git lfs clone ' + cfg['remote-url'] + ' ; '+
+          'tar -cjpf ' + releasesPath + '/' + cfg.target + '/' + cfg['storage-name'] + ' CMSIS_5/* ;' +
+          'rm -Rf /tmp/cmsis'
+          ,
           function(error, stdout, stderr) {
-            console.log(
+            if(error)
+              console.log(stdout+stderr);
+            else 
+              {
+                console.log(stdout);
+                console.log(
                 'Done: ' + releasesPath + '/' + cfg.target + '/' +
                 cfg['storage-name']);
+              }
           });
       break;
     default:
